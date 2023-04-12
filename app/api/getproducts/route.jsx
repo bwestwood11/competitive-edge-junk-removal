4672182a314fea3f48affb42315059faa6af698b
bwestwood11 <dabrettwestwood@gmail.com>
2023-04-11 23:49:25 -0400

    console

--- app/api/getproducts/route.jsx
+++ app/api/getproducts/route.jsx
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(request) {
+   console.log('request', request)
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
      const prices = await stripe.prices.list({
         limit: 4,
