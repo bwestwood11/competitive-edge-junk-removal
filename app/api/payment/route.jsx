69279059b2e9f8477978c298523ba1aadaa26805
bwestwood11 <dabrettwestwood@gmail.com>
2023-04-11 22:54:54 -0400

    changes

--- app/api/payment/route.jsx
+++ app/api/payment/route.jsx
@@ -50,8 +50,8 @@ export async function POST(req) {
       },
     ],
     mode: "payment",
-    success_url: "https://competitiveedgedumpsters.com/success?session_id={CHECKOUT_SESSION_ID}",
-    cancel_url: "https://competitiveedgedumpsters.com/cancel",
+    success_url: 'https://competitiveedgedumpsters.com/success',
+    cancel_url: 'https://competitiveedgedumpsters.com/cancel',
   });
 
   return NextResponse.json(session.url);
