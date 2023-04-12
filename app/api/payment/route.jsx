60e33f1af459050a062e547d8d58c1126fd104fb
bwestwood11 <dabrettwestwood@gmail.com>
2023-04-11 22:03:51 -0400

    changes

--- app/api/payment/route.jsx
+++ app/api/payment/route.jsx
@@ -24,7 +24,7 @@ import Stripe from "stripe";
 // }
 
 export async function POST(req) {
-  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   // const { data } = await req.json();
 
   let data = await req.json();
