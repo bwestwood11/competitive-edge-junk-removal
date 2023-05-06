2185444d4ab8152c1c756c74951b88bbf52387fe
bwestwood11 <dabrettwestwood@gmail.com>
2023-05-05 23:42:20 -0400

    change again

--- app/components/Hero2.jsx
+++ app/components/Hero2.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from "react";
-import image1 from '../../public/image1.jpg';
+import image1 from '../../public/image1.jpg'
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
