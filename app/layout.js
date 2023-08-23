ff6c46e83626550b4f4929f35069822a345680ec
bwestwood11 <dabrettwestwood@gmail.com>
2023-08-22 23:30:48 -0400

    google analytics update

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import './globals.css'
 import { StripeProvider } from './context/StripeContext'
 import Providers from './context/LocalizationProvider'
 import ToastProvider from './context/ToastProvider'
+import GoogleAnalytics from './components/GoogleAnalytics'
+
 
 export const metadata = {
   title: 'Competitive Edge Junk Removal',
@@ -14,7 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <StripeProvider>
+        <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
      <Providers>
       <body>
         <ToastProvider />
