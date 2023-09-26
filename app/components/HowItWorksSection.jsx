b3079c07c346047984c38f0e657165a2f86e255f
bwestwood11 <dabrettwestwood@gmail.com>
2023-09-26 00:12:34 -0400

    changes

--- app/components/HowItWorksSection.jsx
+++ app/components/HowItWorksSection.jsx
@@ -10,7 +10,7 @@ const HowItWorksSection = () => {
           <p className="leading-8 text-gray-600"> You will now click the 'book now' button and this will lead you to a form where we will need some information to fulfill your order. Make sure all your information is correct before submitting. </p>
        </div>
        <div className="mb-4">
-          <p className="leading-8 text-gray-600"> Finally you will checkout with your credit or debit card. If the payment is successfully you will be prompted with a success message. If the payment failed then you will be prompted with a error message. </p>
+          <p className="leading-8 text-gray-600"> Finally you will checkout with your credit or debit card. If the payment is successful you will be prompted with a success message. If the payment failed then you will be prompted with a error message. </p>
        </div>
         <span className="text-3xl font-bold bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent">3. Wait For Delivery Of Your Dumpster</span>
        <p className="text-left mt-3 leading-8 text-gray-600 mb-3">Whatever date and 4 hour time frame you choose will be the delivery date and time of your dumpster. The driver will follow any special instructions you left in the notes. Our driver will call you when he is in route to your specified address. You can give the driver more instructions if need be.</p>
