16b4688cb459bd4d5fbd1618bdb7aa5f96774953
bwestwood11 <dabrettwestwood@gmail.com>
2024-02-12 21:32:37 -0500

    changesss

--- app/api/getproducts/route.jsx
+++ app/api/getproducts/route.jsx
@@ -9,7 +9,7 @@ export async function GET(request) {
         limit: 4,
         active: true,
      });
-     revalidatePath('/pricing')
+     revalidatePath(prices.data)
 
    return NextResponse.json(prices.data) 
 
